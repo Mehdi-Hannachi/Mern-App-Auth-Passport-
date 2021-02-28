2d8e124c21086f584b5f74206da92e52fd3a8334
Mehdi <mehdihannachi02@gmail.com>
2021-02-28 21:31:18 +0100

    some changes

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,10 +1,10 @@
 import React, { Component, useEffect, useState } from "react";
 import { Redirect, Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function PrivateRoute({ component: Component, ...rest }) {
-  //   const isAuth = useSelector((state) => state.userReducer.isAuth);
-  //   console.log(isAuth);
+  const isAuth = useSelector((state) => state.userReducer.isAuth);
+  console.log(isAuth);
 
   if (!localStorage.getItem("token")) {
     return <Redirect to="/login" />;
